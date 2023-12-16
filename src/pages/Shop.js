1750269041329af004414df9c5421342086127ed
Matthew Zujewski <Mattzujewski@MacBook-Pro-2.local>
2023-12-15 20:43:03 -0500

    fixed broken shop.js file

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -5,7 +5,6 @@ import { useNavigate } from 'react-router-dom';
 
 
 
-
 export function Shop() {
   const [storeNames, setStoreNames] = useState([]);
   const [selectedStores, setSelectedStores] = useState([]);
@@ -18,8 +17,6 @@ export function Shop() {
   const [selectedProcessor, setSelectedProcessor] = useState('');
   const [selectedProcessorGen, setSelectedProcessorGen] = useState('');
   const [selectedGraphics, setSelectedGraphics] = useState('');
-  const [selectedForComparison, setSelectedForComparison] = useState([]);
-
 
 
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -149,17 +146,6 @@ export function Shop() {
   //     origin: { y: 0.6 }
   //   });
   // };
-  const handleCompareChange = (product, isChecked) => {
-    setSelectedForComparison(prev => {
-      if (isChecked) {
-        // If the checkbox is checked, add the product to the array
-        return [...prev, product];
-      } else {
-        // If the checkbox is unchecked, remove the product from the array
-        return prev.filter(p => p.ProductID !== product.ProductID);
-      }
-    });
-  };
 
   const buyProduct = async (productId, customerLat, customerLon) => {
     const data = {
@@ -190,6 +176,7 @@ export function Shop() {
       throw error;
     }
   };
+
 
 
   const handleCompareCheck = (productID, isChecked) => {
@@ -427,7 +414,7 @@ if(Array.isArray(products)) {
   const filteredStoreNames = storeNames.filter((name) =>
     name.toLowerCase().includes(searchQuery.toLowerCase())
   );
-  console.log("selectedForComparison",selectedForComparison);
+
   return (
     <div>
       <h1>Shop</h1>
@@ -650,27 +637,9 @@ if(Array.isArray(products)) {
 
 </div>
       </div>
-
+      
 {/* Products Section */}
 <div>
-  {/* Compare Button - Visible when 2 or more products are selected */}
-{selectedForComparison.length >= 2 && (
-  <Link to={{ pathname: "/compare", state: { products: selectedForComparison } }}>
-    <button
-      style={{
-        marginTop: '20px',
-        padding: '10px 20px',
-        background: '#007bff',
-        color: '#fff',
-        borderRadius: '5px',
-        border: 'none',
-        cursor: 'pointer',
-      }}
-    >
-      Compare Products
-    </button>
-  </Link>
-)}
   <h2>Products:</h2>
   <ul style={{ listStyleType: 'none', padding: 20 }}>
     {filteredProducts.map((product, index) => (
@@ -715,6 +684,7 @@ if(Array.isArray(products)) {
             Buy
           </button>
         </div>
+
         {/* Product Image */}
         <div> {/* Flex item for image */}
           <img
@@ -722,17 +692,22 @@ if(Array.isArray(products)) {
             alt="Product Placeholder"
             style={{ width: '150px', height: '150px' }}
           />
-
                   <div>
-       
-            {/* Compare Checkbox */}
-            <div>
-        <input
-          type="checkbox"
-          id={`compareCheckbox-${product.ProductID}`}
-          onChange={(e) => handleCompareChange(product, e.target.checked)}
-        />
-        <label htmlFor={`compareCheckbox-${product.ProductID}`}>Compare</label>
+          <input
+            type="checkbox"
+            id={`compareCheckbox-${product.ProductID}`}
+            onChange={(e) => handleCompareCheck(product.ProductID, e.target.checked)}
+          />
+          <label htmlFor={`compareCheckbox-${product.ProductID}`}></label>
+                    <button
+            onClick={handleCompareClick}
+            disabled={compareProducts.length === 0}
+          >
+            Compare
+          </button>
+        
+
+
       </div>
         </div>
       </li>
@@ -764,4 +739,4 @@ if(Array.isArray(products)) {
 )}
     </div>
   );
-              }  
+              } 
